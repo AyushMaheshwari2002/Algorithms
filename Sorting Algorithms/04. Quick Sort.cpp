ddc3f40b27f3c2b1c7cb412b647e7d0441424eed
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-03-24 10:38:42 +0530

    Create 04. Quick Sort.cpp

---
+++ Sorting Algorithms/04. Quick Sort.cpp
@@ -0,0 +1,92 @@
+/* 
+
+
+
+*/
+
+
+
+#include <iostream>
+using namespace std;
+
+int partition(int arr[], int lb, int ub)
+{
+    int pivot = arr[lb];
+    int start = lb;
+    int end = ub;
+    
+    while(start < end)
+    {
+        while(arr[start] <= pivot)
+        {
+            start++;
+        }
+        while(arr[end] > pivot)
+        {
+            end--;
+        }
+        if(start < end)
+        {
+            swap(arr[start] , arr[end]);
+        }
+    }
+    swap(arr[lb] , arr[end]);
+    
+    return end;
+}
+
+void swap(int &a , int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void quickSort(int arr[], int lb, int ub)
+{
+    if(lb < ub)
+    { 
+    	// partitioning the array
+    	int p = partition(arr, lb , ub);
+    
+    	// Sorting the left part
+    	quickSort(arr, lb , p - 1);
+    
+    	// Sorting the right part
+    	quickSort(arr, p + 1 , ub);
+    }
+}
+
+void display(int arr[] , int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int size;
+    cout << "Enter size of the array : ";
+    cin >> size;
+      
+    int arr[size];
+    
+    cout << "\nEnter the elements in the array : " << endl;
+    for(int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+    
+    cout << "\nArray before Sorting : ";
+    display(arr , size);
+    
+    quickSort(arr , 0 , size-1);
+    
+    cout << "Array after Sorting : ";
+    display(arr , size);
+}
+
+
